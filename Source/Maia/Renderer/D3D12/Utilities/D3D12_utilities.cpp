d7406fd3a6bfaf6140543e62a0c389d2211f8cbe
jpmmaia <jpmmaia@gmail.com>
2018-07-15 16:55:26 +0300

    Migrated to winrt.

--- Source/Maia/Renderer/D3D12/Utilities/D3D12_utilities.cpp
+++ Source/Maia/Renderer/D3D12/Utilities/D3D12_utilities.cpp
@@ -2,50 +2,68 @@
 
 namespace Maia::Renderer::D3D12
 {
-	Microsoft::WRL::ComPtr<IDXGIFactory6> CreateFactory(UINT flags)
+	winrt::com_ptr<IDXGIFactory6> create_factory(UINT flags)
 	{
-		Microsoft::WRL::ComPtr<IDXGIFactory6> factory;
-		CreateDXGIFactory2(flags, IID_PPV_ARGS(&factory));
+		winrt::com_ptr<IDXGIFactory6> factory;
+		CreateDXGIFactory2(flags, __uuidof(factory), factory.put_void());
 		return factory;
 	}
-	Microsoft::WRL::ComPtr<IDXGIAdapter4> SelectAdapter(IDXGIFactory6& factory, bool selectWarpAdapter)
+	winrt::com_ptr<IDXGIAdapter4> select_adapter(IDXGIFactory6& factory, bool select_WARP_adapter)
 	{
-		if (selectWarpAdapter)
+		if (select_WARP_adapter)
 		{
-			Microsoft::WRL::ComPtr<IDXGIAdapter4> warpAdapter;
-			D3D12::ThrowIfFailed(factory.EnumWarpAdapter(IID_PPV_ARGS(&warpAdapter)));
-			return warpAdapter;
+			winrt::com_ptr<IDXGIAdapter4> warp_adapter;
+			winrt::check_hresult(
+				factory.EnumWarpAdapter(__uuidof(warp_adapter), warp_adapter.put_void()));
+
+			return warp_adapter;
 		}
 
 		else
 		{
-			Microsoft::WRL::ComPtr<IDXGIAdapter4> hardwareAdapter;
-			D3D12::ThrowIfFailed(factory.EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&hardwareAdapter)));
-			return hardwareAdapter;
+			winrt::com_ptr<IDXGIAdapter4> hardware_adapter;
+			winrt::check_hresult(
+				factory.EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, __uuidof(hardware_adapter), hardware_adapter.put_void()));
+
+			return hardware_adapter;
 		}
 	}
-	Microsoft::WRL::ComPtr<ID3D12Device4> CreateD3DDevice(IDXGIAdapter& adapter, D3D_FEATURE_LEVEL minimumFeatureLevel)
+	winrt::com_ptr<ID3D12Device4> create_device(IDXGIAdapter& adapter, D3D_FEATURE_LEVEL minimum_feature_level)
 	{
-		Microsoft::WRL::ComPtr<ID3D12Device4> d3dDevice;
-		D3D12::ThrowIfFailed(D3D12CreateDevice(&adapter, minimumFeatureLevel, IID_PPV_ARGS(&d3dDevice)));
-		return d3dDevice;
+		winrt::com_ptr<ID3D12Device4> device;
+		winrt::check_hresult(
+			D3D12CreateDevice(&adapter, minimum_feature_level, __uuidof(device), device.put_void()));
+
+		return device;
 	}
-	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> CreateCommandAllocator(ID3D12Device& d3dDevice, D3D12_COMMAND_LIST_TYPE type)
+	winrt::com_ptr<ID3D12CommandAllocator> create_commandA_allocator(ID3D12Device& device, D3D12_COMMAND_LIST_TYPE type)
 	{
-		Microsoft::WRL::ComPtr<ID3D12CommandAllocator> commandAllocator;
-		D3D12::ThrowIfFailed(d3dDevice.CreateCommandAllocator(type, IID_PPV_ARGS(&commandAllocator)));
-		return commandAllocator;
+		winrt::com_ptr<ID3D12CommandAllocator> command_allocator;
+		winrt::check_hresult(
+			device.CreateCommandAllocator(type, __uuidof(command_allocator), command_allocator.put_void()));
+
+		return command_allocator;
 	}
-	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CreateOpenedGraphicsCommandList(ID3D12Device& d3dDevice, UINT nodeMask, D3D12_COMMAND_LIST_TYPE type, ID3D12CommandAllocator& commandAllocator, ID3D12PipelineState* initialState)
+	winrt::com_ptr<ID3D12GraphicsCommandList> create_opened_graphics_command_list(
+		ID3D12Device& device, UINT node_mask, D3D12_COMMAND_LIST_TYPE type, 
+		ID3D12CommandAllocator& command_allocator, ID3D12PipelineState* initial_state
+	)
 	{
-		Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> commandList;
-		D3D12::ThrowIfFailed(d3dDevice.CreateCommandList(nodeMask, type, &commandAllocator, initialState, IID_PPV_ARGS(&commandList)));
-		return commandList;
+		winrt::com_ptr<ID3D12GraphicsCommandList> command_list;
+		winrt::check_hresult(
+			device.CreateCommandList(node_mask, type, &command_allocator, initial_state, __uuidof(command_list), command_list.put_void()));
+
+		return command_list;
 	}
-	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CreateClosedGraphicsCommandList(ID3D12Device& d3dDevice, UINT nodeMask, D3D12_COMMAND_LIST_TYPE type, ID3D12CommandAllocator& commandAllocator, ID3D12PipelineState* initialState)
+	winrt::com_ptr<ID3D12GraphicsCommandList> create_closed_graphics_command_list(
+		ID3D12Device& device, UINT node_mask, D3D12_COMMAND_LIST_TYPE type,
+		ID3D12CommandAllocator& command_allocator, ID3D12PipelineState* initial_state
+	)
 	{
-		auto commandList = CreateOpenedGraphicsCommandList(d3dDevice, nodeMask, type, commandAllocator, initialState);
-		D3D12::ThrowIfFailed(commandList->Close());
-		return commandList;
+		auto const command_list = create_opened_graphics_command_list(device, node_mask, type, command_allocator, initial_state);
+		winrt::check_hresult(
+			command_list->Close());
+
+		return command_list;
 	}
 }
--- Source/Maia/Renderer/D3D12/Utilities/D3D12_utilities.h
+++ Source/Maia/Renderer/D3D12/Utilities/D3D12_utilities.h
@@ -1,25 +1,16 @@
 #include <stdexcept>
 
-#include <comdef.h>
+#include <winrt/base.h>
+
 #include <d3d12.h>
 #include <dxgi1_6.h>
-#include <wrl/client.h>
 
 namespace Maia::Renderer::D3D12
 {
-	inline void ThrowIfFailed(HRESULT result)
-	{
-		if (FAILED(result))
-		{
-			_com_error error(result);
-			throw std::runtime_error(error.ErrorMessage());
-		}
-	}
-
-	Microsoft::WRL::ComPtr<IDXGIFactory6> CreateFactory(UINT flags);
-	Microsoft::WRL::ComPtr<IDXGIAdapter4> SelectAdapter(IDXGIFactory6& factory, bool selectWarpAdapter);
-	Microsoft::WRL::ComPtr<ID3D12Device4> CreateD3DDevice(IDXGIAdapter& adapter, D3D_FEATURE_LEVEL minimumFeatureLevel);
-	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> CreateCommandAllocator(ID3D12Device& d3dDevice, D3D12_COMMAND_LIST_TYPE type);
-	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CreateOpenedGraphicsCommandList(ID3D12Device& d3dDevice, UINT nodeMask, D3D12_COMMAND_LIST_TYPE type, ID3D12CommandAllocator& commandAllocator, ID3D12PipelineState* initialState = nullptr);
-	Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> CreateClosedGraphicsCommandList(ID3D12Device& d3dDevice, UINT nodeMask, D3D12_COMMAND_LIST_TYPE type, ID3D12CommandAllocator& commandAllocator, ID3D12PipelineState* initialState = nullptr);
+	winrt::com_ptr<IDXGIFactory6> create_factory(UINT flags);
+	winrt::com_ptr<IDXGIAdapter4> select_adapter(IDXGIFactory6& factory, bool select_WARP_adapter);
+	winrt::com_ptr<ID3D12Device4> create_device(IDXGIAdapter& adapter, D3D_FEATURE_LEVEL minimum_feature_level);
+	winrt::com_ptr<ID3D12CommandAllocator> create_commandA_allocator(ID3D12Device& device, D3D12_COMMAND_LIST_TYPE type);
+	winrt::com_ptr<ID3D12GraphicsCommandList> create_opened_graphics_command_list(ID3D12Device& device, UINT node_mask, D3D12_COMMAND_LIST_TYPE type, ID3D12CommandAllocator& command_allocator, ID3D12PipelineState* initial_state = nullptr);
+	winrt::com_ptr<ID3D12GraphicsCommandList> create_closed_graphics_command_list(ID3D12Device& device, UINT node_mask, D3D12_COMMAND_LIST_TYPE type, ID3D12CommandAllocator& command_allocator, ID3D12PipelineState* initial_state = nullptr);
 }
