6dde34750e08a89976f100d9bdf840a1c71f33a7
jpmmaia <jpmmaia@gmail.com>
2018-07-24 18:50:30 +0300

    Added create_fence().

--- Source/Maia/Renderer/D3D12/Utilities/D3D12_utilities.cpp
+++ Source/Maia/Renderer/D3D12/Utilities/D3D12_utilities.cpp
@@ -8,7 +8,7 @@ namespace Maia::Renderer::D3D12
 		CreateDXGIFactory2(flags, __uuidof(factory), factory.put_void());
 		return factory;
 	}
-	winrt::com_ptr<IDXGIAdapter4> select_adapter(IDXGIFactory6& factory, bool select_WARP_adapter)
+	winrt::com_ptr<IDXGIAdapter4> select_adapter(IDXGIFactory6& factory, bool const select_WARP_adapter)
 	{
 		if (select_WARP_adapter)
 		{
@@ -28,7 +28,7 @@ namespace Maia::Renderer::D3D12
 			return hardware_adapter;
 		}
 	}
-	winrt::com_ptr<ID3D12Device4> create_device(IDXGIAdapter& adapter, D3D_FEATURE_LEVEL minimum_feature_level)
+	winrt::com_ptr<ID3D12Device4> create_device(IDXGIAdapter& adapter, D3D_FEATURE_LEVEL const minimum_feature_level)
 	{
 		winrt::com_ptr<ID3D12Device4> device;
 		winrt::check_hresult(
@@ -36,7 +36,7 @@ namespace Maia::Renderer::D3D12
 
 		return device;
 	}
-	winrt::com_ptr<ID3D12CommandQueue> create_command_queue(ID3D12Device& device, D3D12_COMMAND_LIST_TYPE type, INT priority, D3D12_COMMAND_QUEUE_FLAGS flags, UINT node_mask)
+	winrt::com_ptr<ID3D12CommandQueue> create_command_queue(ID3D12Device& device, D3D12_COMMAND_LIST_TYPE const type, INT const priority, D3D12_COMMAND_QUEUE_FLAGS const flags, UINT const node_mask)
 	{
 		D3D12_COMMAND_QUEUE_DESC description;
 		description.Type = type;
@@ -49,7 +49,7 @@ namespace Maia::Renderer::D3D12
 
 		return command_queue;
 	}
-	winrt::com_ptr<ID3D12CommandAllocator> create_command_allocator(ID3D12Device& device, D3D12_COMMAND_LIST_TYPE type)
+	winrt::com_ptr<ID3D12CommandAllocator> create_command_allocator(ID3D12Device& device, D3D12_COMMAND_LIST_TYPE const type)
 	{
 		winrt::com_ptr<ID3D12CommandAllocator> command_allocator;
 		winrt::check_hresult(
@@ -58,8 +58,8 @@ namespace Maia::Renderer::D3D12
 		return command_allocator;
 	}
 	winrt::com_ptr<ID3D12GraphicsCommandList> create_opened_graphics_command_list(
-		ID3D12Device& device, UINT node_mask, D3D12_COMMAND_LIST_TYPE type, 
-		ID3D12CommandAllocator& command_allocator, ID3D12PipelineState* initial_state
+		ID3D12Device& device, UINT const node_mask, D3D12_COMMAND_LIST_TYPE const type, 
+		ID3D12CommandAllocator& command_allocator, ID3D12PipelineState* const initial_state
 	)
 	{
 		winrt::com_ptr<ID3D12GraphicsCommandList> command_list;
@@ -69,8 +69,8 @@ namespace Maia::Renderer::D3D12
 		return command_list;
 	}
 	winrt::com_ptr<ID3D12GraphicsCommandList> create_closed_graphics_command_list(
-		ID3D12Device& device, UINT node_mask, D3D12_COMMAND_LIST_TYPE type,
-		ID3D12CommandAllocator& command_allocator, ID3D12PipelineState* initial_state
+		ID3D12Device& device, UINT const node_mask, D3D12_COMMAND_LIST_TYPE const type,
+		ID3D12CommandAllocator& command_allocator, ID3D12PipelineState* const initial_state
 	)
 	{
 		auto const command_list = create_opened_graphics_command_list(device, node_mask, type, command_allocator, initial_state);
@@ -79,7 +79,7 @@ namespace Maia::Renderer::D3D12
 
 		return command_list;
 	}
-	winrt::com_ptr<ID3D12DescriptorHeap> create_descriptor_heap(ID3D12Device& device, D3D12_DESCRIPTOR_HEAP_TYPE type, UINT num_descriptors, D3D12_DESCRIPTOR_HEAP_FLAGS flags, UINT node_mask)
+	winrt::com_ptr<ID3D12DescriptorHeap> create_descriptor_heap(ID3D12Device& device, D3D12_DESCRIPTOR_HEAP_TYPE const type, UINT const num_descriptors, D3D12_DESCRIPTOR_HEAP_FLAGS const flags, UINT const node_mask)
 	{
 		D3D12_DESCRIPTOR_HEAP_DESC description;
 		description.Type = type;
@@ -88,8 +88,17 @@ namespace Maia::Renderer::D3D12
 		description.NodeMask = node_mask;
 
 		winrt::com_ptr<ID3D12DescriptorHeap> descriptor_heap;
-		device.CreateDescriptorHeap(&description, __uuidof(descriptor_heap), descriptor_heap.put_void());
+		winrt::check_hresult(
+			device.CreateDescriptorHeap(&description, __uuidof(descriptor_heap), descriptor_heap.put_void()));
 		
 		return descriptor_heap;
+	}
+	winrt::com_ptr<ID3D12Fence> create_fence(ID3D12Device& device, UINT64 const initial_value, D3D12_FENCE_FLAGS const flags)
+	{
+		winrt::com_ptr<ID3D12Fence> fence;
+		winrt::check_hresult(
+			device.CreateFence(initial_value, flags, __uuidof(fence), fence.put_void()));
+
+		return fence;
 	}
 }
