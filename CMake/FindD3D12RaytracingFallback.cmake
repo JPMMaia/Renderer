f36a6875332e8e9e6e8bc6a92cda14528f339715
jpmmaia <jpmmaia@gmail.com>
2018-08-12 20:43:28 +0300

    Adding dependency on D3D12RaytracingFallback.

--- CMake/FindD3D12RaytracingFallback.cmake
+++ CMake/FindD3D12RaytracingFallback.cmake
@@ -1,14 +1,46 @@
-find_path (D3D12RaytracingFallback_INCLUDE_DIR "DirectX-Graphics-Samples/Libraries/D3D12RaytracingFallback/Include/D3D12RaytracingFallback.h")
+include (FetchContent)
+
+FetchContent_Declare (
+    DirectXRaytracingBinaries
+    URL "https://github.com/Microsoft/DirectX-Graphics-Samples/releases/download/v1.3-dxr/DirectXRaytracingBinariesV1.3.zip"
+)
+
+FetchContent_GetProperties (DirectXRaytracingBinaries)
+if (NOT directxraytracingbinaries_POPULATED)
+	FetchContent_Populate (DirectXRaytracingBinaries)
+endif ()
+
+
+FetchContent_Declare (
+    DirectXRaytracingSource
+    URL "https://github.com/Microsoft/DirectX-Graphics-Samples/archive/v1.3-dxr.zip"
+)
+
+FetchContent_GetProperties (DirectXRaytracingSource)
+if (NOT directxraytracingsource_POPULATED)
+    FetchContent_Populate (DirectXRaytracingSource)
+endif ()
+
+find_path (D3D12RaytracingFallback_INCLUDE_DIR "D3D12RaytracingFallback.h" PATHS "${directxraytracingsource_SOURCE_DIR}/Libraries/D3D12RaytracingFallback/Include" NO_DEFAULT_PATH)
 mark_as_advanced (D3D12RaytracingFallback_INCLUDE_DIR)
+
+find_path (D3D12RaytracingFallback_LOCATION "dxrfallbackcompiler.dll" PATHS "${directxraytracingbinaries_SOURCE_DIR}" NO_DEFAULT_PATH)
+mark_as_advanced (D3D12RaytracingFallback_LOCATION)
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (D3D12RaytracingFallback
-    REQUIRED_VARS D3D12RaytracingFallback_INCLUDE_DIR
+    REQUIRED_VARS 
+        D3D12RaytracingFallback_INCLUDE_DIR
+        D3D12RaytracingFallback_LOCATION 
 )
 
 if (D3D12RaytracingFallback_FOUND AND NOT TARGET D3D12::RaytracingFallback)
+    
     add_library (D3D12::RaytracingFallback UNKNOWN IMPORTED)
-    set_target_properties (D3D12::RaytracingFallback PROPERTIES
+
+    set_target_properties(D3D12::RaytracingFallback PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES "${D3D12RaytracingFallback_INCLUDE_DIR}"
+        IMPORTED_LOCATION "${D3D12RaytracingFallback_LOCATION}"
     )
+    
 endif ()
