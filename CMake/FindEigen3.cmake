48ae40e41c17938e200cb71f35d6f380b1bfde80
JPMMaia <jpmmaia@gmail.com>
2018-09-15 23:46:03 +0300

    Updating FindEigen3.cmake.

--- CMake/FindEigen3.cmake
+++ CMake/FindEigen3.cmake
@@ -1,26 +1,21 @@
-include (FetchContent)
+find_package (Eigen3 3.3 NO_MODULE)
 
-FetchContent_Declare (
-    Eigen3
-    URL "http://bitbucket.org/eigen/eigen/get/3.3.5.zip"
-)
+if (NOT TARGET Eigen3::Eigen)
 
-FetchContent_GetProperties (Eigen3)
-if (NOT eigen3_POPULATED)
-    FetchContent_Populate (Eigen3)
-endif ()
+    include (FetchContent)
+    FetchContent_Declare (
+        Eigen3
+        URL "http://bitbucket.org/eigen/eigen/get/3.3.5.zip"
+    )
 
-find_path (Eigen3_INCLUDE_DIR "Eigen/Eigen" PATHS "${eigen3_SOURCE_DIR}" NO_DEFAULT_PATH)
-mark_as_advanced (Eigen3_INCLUDE_DIR)
+    FetchContent_GetProperties (Eigen3)
+    if (NOT eigen3_POPULATED)
 
-include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (Eigen3
-    REQUIRED_VARS 
-        Eigen3_INCLUDE_DIR
-)
+        FetchContent_Populate (Eigen3)
 
-if (Eigen3_FOUND AND NOT TARGET Eigen3::Eigen)
-    
-    add_subdirectory("${eigen3_SOURCE_DIR}" "${eigen3_BINARY_DIR}" EXCLUDE_FROM_ALL)
-    
+        list (APPEND CMAKE_MODULE_PATH "${eigen3_SOURCE_DIR}")
+        find_package (Eigen3 3.3 REQUIRED NO_MODULE)
+
+    endif()
+
 endif ()
