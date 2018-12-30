b336de75ac7fa62888c1af675f881a5bc4dfc27d
João Maia <jpmmaia@gmail.com>
2018-12-30 16:30:49 +0200

    Feature/view and projection matrices (#1)
    Implemented functions to create perspective and orthographic projection matrices and view matrix.

---
+++ Source/Maia/Renderer/Matrices.cpp
@@ -0,0 +1,52 @@
+#include <cassert>
+
+#include "Matrices.hpp"
+
+namespace Maia::Renderer
+{
+	Eigen::Matrix4f create_view_matrix(Eigen::Vector3f const& position, Eigen::Quaternionf const& rotation)
+	{
+		assert(rotation.isApprox(rotation.normalized()));
+
+		Eigen::Matrix4f value;
+		value.topLeftCorner<3, 3>() = rotation.conjugate().toRotationMatrix();
+		value.topRightCorner<3, 1>() = value.topLeftCorner<3, 3>() * (-position);
+		value.bottomLeftCorner<1, 4>() = Eigen::Vector4f{ 0.0f, 0.0f, 0.0f, 1.0f };
+		return value;
+	}
+
+	Eigen::Matrix4f create_orthographic_projection_matrix(Eigen::Vector3f const& dimensions)
+	{
+		Eigen::Matrix4f value;
+		value << 
+			2.0f / dimensions(0), 0.0f, 0.0f, 0.0f,
+			0.0f, 2.0f / dimensions(1), 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f / dimensions(2), 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f;
+		return value;
+	}
+
+	Eigen::Matrix4f create_perspective_projection_matrix(Eigen::Vector2f const& half_dimensions, Eigen::Vector2f const& zRange)
+	{
+		float const right = half_dimensions(0);
+		float const bottom = half_dimensions(1);
+		float const near = zRange(0);
+		float const far = zRange(1);
+
+		Eigen::Matrix4f value;
+		value <<
+			near / right, 0.0f, 0.0f, 0.0f,
+			0.0f, near / bottom, 0.0f, 0.0f,
+			0.0f, 0.0f, far / (far - near), - far * near / (far - near),
+			0.0f, 0.0f, 1.0f, 0.0f;
+		return value;
+	}
+
+	Eigen::Matrix4f create_perspective_projection_matrix(float const vertical_half_angle_of_view, float const width_by_height_ratio, Eigen::Vector2f const& zRange)
+	{
+		float const bottom = std::tan(vertical_half_angle_of_view) * zRange(0);
+		float const right = width_by_height_ratio * bottom;
+
+		return create_perspective_projection_matrix({ right, bottom }, zRange);
+	}
+}
